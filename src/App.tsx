d6ae5a783affdcba1f13122782943d35ab6b230a
AYRESRIO <AYRESRIO@HOTMAIL.COM>
2025-02-18 00:16:18 -0300

    Layout component Listing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-
-import './App.css'
-import Filter from './components/Filter';
-import Header from './components/Header'
+import "./App.css";
+import Filter from "./components/Filter";
+import Header from "./components/Header";
+import Listing from "./components/Listing";
 
 export default function App() {
   return (
-
     <>
-    <Header />
-    <Filter />
+      <Header />
+      <Filter />
+      <Listing />
     </>
-    
-);
+  );
 }
-
---
+++ src/components/Listing/index.tsx
@@ -0,0 +1,64 @@
+import "./styles.css";
+export default function Listing() {
+  return (
+    <>
+      <div className="DSF-container DSF-listing">
+
+        <div className="DSF-listing-item">
+          <div className="DSF-listing-fonts">
+            <p>PC Gamer Pro</p>
+          </div>
+          <div className="DSF-listing-price">
+            <h3>R$ 1200.00</h3>
+          </div>
+        </div>
+
+        <div className="DSF-listing-item">
+          <div className="DSF-listing-fonts">
+            <p>PC Gamer Pro</p>
+          </div>
+          <div className="DSF-listing-price">
+            <h3>R$ 1200.00</h3>
+          </div>
+        </div>
+
+        <div className="DSF-listing-item">
+          <div className="DSF-listing-fonts">
+            <p>PC Gamer Pro</p>
+          </div>
+          <div className="DSF-listing-price">
+            <h3>R$ 1200.00</h3>
+          </div>
+        </div>
+
+        <div className="DSF-listing-item">
+          <div className="DSF-listing-fonts">
+            <p>PC Gamer Pro</p>
+          </div>
+          <div className="DSF-listing-price">
+            <h3>R$ 1200.00</h3>
+          </div>
+        </div>
+
+        <div className="DSF-listing-item">
+          <div className="DSF-listing-fonts">
+            <p>PC Gamer Pro</p>
+          </div>
+          <div className="DSF-listing-price">
+            <h3>R$ 1200.00</h3>
+          </div>
+        </div>
+
+        <div className="DSF-listing-item">
+          <div className="DSF-listing-fonts">
+            <p>PC Gamer Pro</p>
+          </div>
+          <div className="DSF-listing-price">
+            <h3>R$ 1200.00</h3>
+          </div>
+        </div>
+
+      </div>
+    </>
+  );
+}
